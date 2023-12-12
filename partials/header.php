c24a7ebc1ac1e71a1bdcaca0d15eb4501eb2a4f6
Ogunsanya Taoeeq <taofeeq.muhammad22@gmail.com>
2023-12-12 18:02:04 +0100

    Uploading ora files

---
+++ partials/header.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog</title>
+    <!-- custom style sheet  -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+
+<body>
+    <nav>
+        <div class="container nav_container">
+            <a href="index.php" class="nav_logo"> NEWSUPDA8</a>
+            <ul class="nav_items">
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <!-- <li><a href="signin.php">Signin</a></li> -->
+                <li class="nav_profile">
+                    <div class="avatar">
+                        <img src="./img/nodp.png">
+                    </div>
+                    <ul>
+                        <li><a href="admin/index.php">Dashboard</a></li>
+                        <li><a href="logout">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <button id="open_nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close_nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
+    <!-- ******************** End of Nav Section *********************** -->
